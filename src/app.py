8fbe68509342d827212c5e25f2efcb710d836fb3
Fer <yilmazferhat1986@gmail.com>
2019-12-11 21:50:54 -0500

    Feature : Edited some files, created migrations

--- src/app.py
+++ src/app.py
@@ -1,6 +1,12 @@
+# src/app.py
+
 from flask import Flask
 
 from .config import app_config
+from .models import db, bcrypt
+
+# import user_api blueprint
+from .views.UserView import user_api as user_blueprint
 
 
 def create_app(env_name):
@@ -13,11 +19,17 @@ def create_app(env_name):
 
     app.config.from_object(app_config[env_name])
 
+    # initializing bcrypt and db
+    bcrypt.init_app(app)
+    db.init_app(app)
+
+    app.register_blueprint(user_blueprint, url_prefix='/api/v1/users')
+
     @app.route('/', methods=['GET'])
     def index():
         """
         example endpoint
         """
-        return 'Congratulations! Your first endpoint is workin'
+        return 'Congratulations! Your part 2 endpoint is working'
 
     return app
--- src/models/__init__.py
+++ src/models/__init__.py
@@ -6,3 +6,6 @@ from flask_bcrypt import Bcrypt
 # initialize our db
 db = SQLAlchemy()
 bcrypt = Bcrypt()
+
+from .BlogpostModel import BlogpostModel, BlogpostSchema
+from .UserModel import UserModel, UserSchema
